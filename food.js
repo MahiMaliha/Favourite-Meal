380f3e479b89ff61c3d13a5670c422438fc7346c
Jannatul Mahiya Mutasha <87617184+JannatulMutashaEngineer@users.noreply.github.com>
2021-09-15 17:47:43 +0600

    Add files via upload

--- food.js
+++ food.js
@@ -28,7 +28,7 @@ div.innerHTML = `
     <p class="card-text text-center">${food.strInstructions.slice(0,200)}</p>
   </div>
   <div class="card-footer">
-  <button type="button" class="btn btn-outline-secondary item-button">See Details</button>
+  <button onclick = "singleFood(${food.idMeal})" type="button" class="btn btn-outline-secondary item-button">See Details</button>
   
 </div>
 </div>
@@ -38,3 +38,31 @@ displayItem.appendChild(div)
 
     }
 }
+const singleFood = (itemId) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemId}`;
+    fetch(url)
+     .then(res => res.json())
+     .then(data => displaySingleMeal(data.meals[0]));
+ }
+
+ const displaySingleMeal = meal => {
+
+const showItem = document.getElementById('single-item');
+const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML=`
+    <div onclick="showDetails(${meal.idMeal})" class="card h-100 w-25 p-2 mx-auto mt-4">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions.slice(0,150)}</p>
+    </div>
+    <button class=" btn btn-outline-primary w-50 mx-auto mb-4">Go To Youtube</button>
+  </div>
+`
+showItem.appendChild(div);
+
+ }
+    
+    
+    
